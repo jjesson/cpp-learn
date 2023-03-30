26fa24c5186115fef19924f12b0f2aba3fcd727f
jjesson <65348606+jjesson@users.noreply.github.com>
2023-03-30 14:12:02 -0500

    Create metronome.cpp

---
+++ metronome.cpp
@@ -0,0 +1,17 @@
+//This program is designed to tell you how many winds you need to wind to end on a song with a 4/4 timescale
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float winds;
+    
+    cin >> winds;
+    
+    winds = winds / 4;
+    
+    cout << winds;
+}

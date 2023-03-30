d0a36f4e9fb98cbb6bf57ef033881066e4a1691c
jjesson <65348606+jjesson@users.noreply.github.com>
2023-03-30 09:29:35 -0500

    Create program1-hello-world.cpp

---
+++ program1-hello-world.cpp
@@ -0,0 +1,14 @@
+// This is how to write a comment.
+
+//This is how all programs should be started in C++, denoted by xxx color
+#include <iostream>
+
+int main()
+{
+  //We use std:: because it is a standard command in c++
+  std:: cout<<"Hello World";
+  //cout is the equivalent to a print statement.
+  
+  //We need to return a 0 because we started the program with int (integer)
+  return 0;
+}
